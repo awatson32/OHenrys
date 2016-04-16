f2145dc3274aa7defa9c565f824d6e9fae1b28f2
awatson32 <audrey.watson32@gmail.com>
2016-04-16 15:12:06 -0500

    Added images and formatting to home

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -3,8 +3,19 @@ import BaseViewControl from '../base/base.vc';
 
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
+    
+    // constructor(navbar: platui.Navbar) {
+    //     super();
+    // }
 
-    context: any = {};
+    // context: any = {};
+    
+    // navbar.setLeft({
+    //     content: '<i class="facebook"></i>',
+    //     action() => {
+    //         alert('Going to Facebook');
+    //     }
+    // });
 }
 
 register.viewControl('home-vc', HomeViewControl);
